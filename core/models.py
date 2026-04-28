5606d4d67e813ddede69803149d81e52b938586d
Marco Wolff <marcowolff619@gmail.com>
2026-04-28 10:55:09 -0400

    Add basic password manager with website links

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Credential(models.Model):
+    website_name = models.CharField(max_length=255)
+    website_url = models.URLField()
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["website_name"]
+
+    def __str__(self) -> str:
+        return f"{self.website_name} ({self.username})"
